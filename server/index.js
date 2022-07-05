82a6bf8d183f93d762a1f52a76ccef301c0feeb7
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-07-05 17:33:41 +0900

    ESlint,prettier 설정

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 //commonJS 진영의 모듈 사용 방법
 //(module.exports, exports.모듈이름)
 //(require 함수)
-//하드코딩 때 대문자 
+//하드코딩 때 대문자
 const server = require('live-server'); //live-server module
 
 const params = {
@@ -10,7 +10,10 @@ const params = {
   port: 3000,
   root: 'client',
   open: false,
-  cors: true
-}
+  cors: true,
+};
 
 server.start(params);
+
+//Linting(코드 검사, 오류 확인) -ESLint
+//formatting (코드 일관성 유지) 추천 -prettier
