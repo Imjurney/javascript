44852ee9c1df13647bfc3e8d667e17ed562f9caf
Ji-yeon CHOI <jurneyx2@gmail.com>
2022-07-21 14:05:45 +0900

    순방향 역방향

--- client/chapter/07-04-nullish.js
+++ client/chapter/07-04-nullish.js
@@ -19,6 +19,14 @@ receivedEmailAddress = (emailAddress === undefined || emailAddress === null) ? '
 receivedEmailAddress = emailAddress ?? 'user@company.io';
 console.log(receivedEmailAddress);
 
-/* ?? vs. || ----------------------------------------------------------- */
+/* ?? vs. || ----------------------------------------------------------- *
 // || → 첫번째 Truthy 값을 반환
-// ?? → 첫번째 정의된(defined) 값을 반환
+// ?? → 첫번째 정의된(defined) 값을 반환*/
+function defaultArg(wantValue, initialValue) {
+  return wantValue ?? initialValue;
+}
+const INITIAL_VALUE = '10px';
+let value;
+
+value = defaultArg(null, INITIAL_VALUE);
+console.log('value =', value);
---
+++ client/chapter/08-1-whileLoop.js
@@ -0,0 +1,93 @@
+/* ---------------------------------------------------------------------- */
+/* Loop                                                                   */
+/* ---------------------------------------------------------------------- */
+//Array(special Object)
+//'length'
+//key(string) indexing(0,1,2,3...)
+
+
+const frontEndDev = [
+  'HTML',
+  'CSS',
+  'SVG',
+  'JavaScript',
+  'jQuery',
+  'React',
+  'Redux',
+];
+
+/* 프론트엔드 개발 집합 항목 출력 ---------------------------------------------- */
+
+// console.log(frontEndDev[0]);
+// console.log(frontEndDev[1]);
+// console.log(frontEndDev[2]);
+// console.log(frontEndDev[3]);
+// console.log(frontEndDev[4]);
+// console.log(frontEndDev[5]);
+
+
+/* 프론트엔드 개발 집합을 순환해서 각 아이템을 Console 패널에 출력 -------------------- */
+
+
+// while 문(순환 : 순방향)
+// let index = 0;
+// let fedindex = frontEndDev.length;
+// while (index < fedindex) {
+//   let value = frontEndDev[index];
+//   index += 1;
+//   console.log(value);
+// }
+
+//역방향 출력하기
+// let lastIndex = frontEndDev.length - 1;
+
+// while (lastIndex >= 0) {
+//   let value = frontEndDev[lastIndex];
+//   lastIndex -= 1
+//   console.log(value);
+// }
+
+// let i = 0;
+// while (frontEndDev[i]) {
+//   console.log(frontEndDev[i]);
+//   ++i;
+// }
+const copyData = frontEndDev.slice();
+
+while (frontEndDev.length) {
+  console.log(frontEndDev.pop());
+}
+
+//
+function checkPerformance() {
+  let time0 = performance.now();
+
+  let result = (performance.now() - time0).toFixed(4);;
+  console.log(`처리 시간 :${result}`);
+  return result;
+
+}
+
+
+// do ~ while 문
+
+
+// for 문
+
+
+/* 객체 순환 -------------------------------------------------------------- */
+
+
+const javaScript = {
+  creator: 'Brendan Eich',
+  createAt: '1995.05',
+  standardName: 'ECMAScript',
+  currentVersion: 2022,
+};
+
+
+// for ~ in 문
+
+
+// for ~ of 문
+// array → object 변환
